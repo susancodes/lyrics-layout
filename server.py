a7257f9856679794312233f261ef7b8556874b5a
Susan <susancodes@gmail.com>
2015-10-02 20:19:37 -0700

    added server.py file

--- server.py
+++ server.py
@@ -8,3 +8,24 @@ echonest_secret = os.environ["ECHONEST_SECRET"]
 app = Flask(__name__)
 
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
+
+@app.route("/")
+def homepage():
+	"""Display webapp."""
+
+	pass
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	connect_to_db(app)
+	PORT = int(os.environ.get("PORT", 5000))
+	DebugToolbarExtension(app)
+
+	DEBUG = "NO_DEBUG" not in os.environ
+
+	app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
